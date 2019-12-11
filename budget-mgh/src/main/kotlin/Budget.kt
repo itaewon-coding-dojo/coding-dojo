e034c9310f08926afebe062135aa82d62b5482cb
noah <noahlog@naver.com>
2019-12-11 21:30:09 +0900

    Init project

---
+++ budget-mgh/src/main/kotlin/Budget.kt
@@ -0,0 +1,11 @@
+fun SupportMaxDepartment(applyBudget: IntArray, budget: Int): Int {
+    var count = 0;
+    applyBudget.sortedArray().fold(budget, { acc, it ->
+        println(acc-it)
+        if(acc-it >= 0 ){
+            count += 1;
+        }
+        acc - it
+    })
+    return count
+}
